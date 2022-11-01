c32e08f64836100a7ae55e45e869df5e11889604
Gabriel Silverio <gabrielriibeiro@gmail.com>
2022-11-01 13:56:40 +0000

    creating attributes and skills models

--- attributes/models.py
+++ attributes/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Attribute(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    strength = models.PositiveIntegerField()
+    agility = models.PositiveIntegerField()
+    intelligence = models.PositiveIntegerField()
+    endurance = models.PositiveIntegerField()
+    
--- skills/models.py
+++ skills/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=127)
+    required_level = models.PositiveIntegerField()
+    description = models.TextField()
+    
