40bd03f11e1e230d2601b048b960bfd896812ded
Debora Correa <deboragomescorr@gmail.com>
2022-11-01 19:21:18 -0300

    feat: create account login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from accounts import views
 
 urlpatterns = [
     path("accounts/", views.CreateAccountView.as_view()),
+    path("accounts/login/", ObtainAuthToken.as_view()),
 ]
