41c4cb9cc977ce9c1253c6c47ac929035b491931
matheuszeiser <matheuszaizer@hotmail.com>
2022-11-01 10:59:58 -0300

    feat: create model characters

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+import uuid
+
+
+class Character(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    nick_name = models.CharField(max_length=50, unique=True)
+    level = models.PositiveIntegerField(default=1, max_digits=2)
+    health = models.PositiveIntegerField(default=100, max_digits=3)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    account = models.ForeignKey(
+        "accounts.Account", on_delete=models.CASCADE, related_name="Character"
+    )
+    inventory = models.OneToOneField(
+        "inventories.Inventory", on_delete=models.CASCADE, related_name="Character"
+    )
+    category = models.ForeignKey(
+        "categories.Category", on_delete=models.CASCADE, related_name="Character"
+    )
+    atributes = models.OneToOneField(
+        "attributes.Attribute", on_delete=models.CASCADE, related_name="Character"
+    )
