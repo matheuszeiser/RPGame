02db2621a5bfc11a0d525ae7963295196e098412
Gabriel Silverio <gabrielriibeiro@gmail.com>
2022-11-03 14:33:52 +0000

    weapon views created

--- weapons/serializer.py
+++ weapons/serializer.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 from rest_framework import serializers
 
 from weapons.models import Weapon
@@ -7,3 +8,11 @@ class WeaponSerializer(serializers.ModelSerializer):
     class Meta:
         model = Weapon
         fields = ['id', 'name', 'damage', 'category', 'inventory']
+
+        read_only_fields = ["inventory"]
+
+    def validate_name(self, value):
+        name =  Weapon.objects.filter(name=value).first()
+        if name:
+            raise serializers.ValidationError("This weapon has already been created")
+        return value
