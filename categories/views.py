ee56c9a5ff9da1c1eb55b2e82c8748b29d6cb55a
Gabriel Silverio <gabrielriibeiro@gmail.com>
2022-11-03 16:58:51 +0000

    category view[list] created and token authorization  added to  category admin route

--- categories/views.py
+++ categories/views.py
@@ -1,11 +1,20 @@
 from rest_framework import generics
 from .models import Category
 from .serializers import CategorySerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAdminUser
 from skills.models import Skill
 import ipdb
 
 
 class CreateCategoryView(generics.CreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
 
+    serializer_class = CategorySerializer
+    queryset = Category.objects
+
+
+class ListCategoriesView(generics.ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects
