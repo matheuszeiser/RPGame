850f6e0e3e092fc785cb1d358676eb64229a3bd1
matheuszeiser <matheuszaizer@hotmail.com>
2022-11-03 15:16:57 -0300

    Added postgres how default database

--- skills/models.py
+++ skills/models.py
@@ -1,9 +1,9 @@
 import uuid
 from django.db import models
 
+
 class Skill(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=127)
     required_level = models.PositiveIntegerField()
     description = models.TextField()
-    
