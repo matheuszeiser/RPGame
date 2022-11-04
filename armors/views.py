968dd59d4d3e1f21d81dc28f598e455484a06f29
Gabriel Silverio <gabrielriibeiro@gmail.com>
2022-11-04 16:17:23 +0000

    armor detail  view created

--- armors/views.py
+++ armors/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from armors.models import Armor
+from .serializer import ArmorSerializer
+
+class ArmorDetailView(generics.UpdateAPIView, generics.DestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+
+    serializer_class = [ArmorSerializer]
+    queryset = Armor.objects
