88b52e846689a2ccae99494ddcf1a4f9227c72ea
KAeHM <samuelsclima03@gmail.com>
2022-11-04 11:28:16 -0300

    feat: adding feature to add itens on inventory

--- characters/urls.py
+++ characters/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path("char/", views.CreateListCharacterView.as_view()),
     path("char/<pk>/", views.RetrieveUpdateDeleteCharView.as_view()),
+    path("char/<char_id>/weapon/<weapon_id>/",
+         views.AddWeaponInInventoryView.as_view()),
+    path("char/<char_id>/weapon/<armor_id>/",
+         views.AddArmorInInventoryView.as_view()),
 ]
